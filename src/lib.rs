198c00d50cad4f17c6b0959bf09b437cb6c6c7a1
Devajit Asem <asem.devajit@gmail.com>
2020-03-08 23:11:49 -0400

    Add zmq

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::io;
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::net::{TcpListener, TcpStream};
 
 pub struct Storage<K, V> {
     data: HashMap<K, V>,
@@ -19,5 +21,58 @@ where K: Hash + Eq,
 
     pub fn put(&mut self, key: K, value: V) {
         self.data.insert(key, value);
+    }
+}
+
+
+// ======================== Node =============================
+
+enum NodeState {
+    Follower,
+    Candidate,
+    Leader,
+}
+
+struct Node<'a> {
+    state: NodeState,
+    term: u64,
+    rpc: &'a NodeRpc,
+}
+
+// ======================== NodeRpc =============================
+
+struct NodeRpc {
+    address: String,
+}
+
+impl NodeRpc {
+    pub fn new(address: String) -> NodeRpc {
+        NodeRpc { address }
+    }
+
+    fn handle_connection(&self, stream: TcpStream) {
+        println!("{:?}", stream);
+    }
+
+    pub fn start(&self) -> io::Result<()> {
+        let listener = TcpListener::bind(self.address.clone())?;
+
+        for stream in listener.incoming() {
+            self.handle_connection(stream?);
+        }
+        Ok(())
+    }
+}
+
+
+// ======================== Tests ========================
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_noderpc() {
+        let rpc = NodeRpc::new("127.0.0.0:8080".to_string());
+        rpc.start();
     }
 }
