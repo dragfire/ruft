1769b6be073fafbcdf66c492301f18f896ae28d8
dragfire <asem.devajit@gmail.com>
2020-04-11 17:25:13 -0400

    Add handlers

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,21 +1,41 @@
 use std::thread;
 use zmq::Socket;
+use std::collections::HashMap;
+
+pub struct message;
+
+type handle = fn() -> i32;
 
 pub struct NodeRpc {
     pub address: String,
     pub client: Socket,
+    pub handlers: HashMap<String, handle>,
 }
+
+fn check() -> i32 {
+    println!("Check...");
+    1
+}
+
+fn register_handlers(rpc: &mut NodeRpc) {
+    let h: handle = check;
+    rpc.handlers.insert("/hello".to_string(), h);
+}
+
 
 impl NodeRpc {
     pub fn new(address: String) -> Result<NodeRpc, zmq::Error> {
         let context = zmq::Context::new();
         let client = context.socket(zmq::REQ)?;
         client.connect(&address)?;
+        let handlers = HashMap::new();
 
-        Ok(NodeRpc { address, client })
+        Ok(NodeRpc { address, client, handlers })
     }
 
-    pub fn start(&self) {
+    pub fn start(&mut self) {
+        register_handlers(self);
+
         thread::spawn(|| {
             let context = zmq::Context::new();
             let server = context.socket(zmq::REP).unwrap();
@@ -40,10 +60,10 @@ mod tests {
 
     #[test]
     fn test_noderpc() {
-        let rpc = NodeRpc::new(String::from("tcp://localhost:5555"));
+        let mut rpc = NodeRpc::new(String::from("tcp://localhost:5555"));
 
         match rpc {
-            Ok(rpc) => {
+            Ok(ref mut rpc) => {
                 rpc.start();
 
                 let mut msg = zmq::Message::new();
@@ -54,6 +74,9 @@ mod tests {
 
                     rpc.client.recv(&mut msg, 0).unwrap();
                     println!("Received Raft {}: {}", msg.as_str().unwrap(), request_nbr);
+                }
+                if let Some(her) = rpc.handlers.get("/hello") {
+                    assert_eq!(her(), 4);
                 }
             },
             Err(_) => println!("Something went wrong")
