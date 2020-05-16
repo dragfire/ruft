b85a55f73402080b90076acf2a189e9073438b59
Devajit Asem <asem.devajit@gmail.com>
2020-05-16 13:30:39 -0400

    Add Channel type

--- src/ticker.rs
+++ src/ticker.rs
@@ -1,23 +1,27 @@
 use std::thread;
 use std::sync::mpsc;
 use std::time::Duration;
+use crate::util;
+
+pub type Channel<T> = (mpsc::Sender<T>, mpsc::Receiver<T>);
 
 pub struct Ticker {
-    duration: Duration,
-    channel: mpsc::Receiver<bool>,
+    pub duration: Duration,
+    pub channel: Channel<bool>,
 }
 
 impl Ticker {
     pub fn new(duration: Duration) -> Self {
-        let (tx, rx) = mpsc::channel();
+        let channel: Channel<bool> = mpsc::channel();
+        let tx1 = channel.0.clone();
         let ticker = Ticker {
             duration,
-            channel: rx,
+            channel,
         };
 
         thread::spawn(move || {
             loop {
-                tx.send(true).unwrap();
+                tx1.send(true).unwrap();
                 thread::sleep(duration);
             }
         });
@@ -32,14 +36,27 @@ mod tests {
 
     #[test]
     fn test_ticker() {
-        let ticker = Ticker::new(Duration::from_millis(100));
+        let ticker = Ticker::new(Duration::from_millis(util::random_timeout()));
+        let tx1 = ticker.channel.0.clone();
+
+        thread::spawn( move || {
+            thread::sleep(Duration::from_secs(2));
+            tx1.send(false).unwrap();
+        });
+
         loop {
-            match ticker.channel.recv() {
-                Ok(_) => {
-                    println!("OK");
+            match ticker.channel.1.recv() {
+                Ok(res) => {
+                    if res {
+                        println!("OK");
+                    } else {
+                        println!("DONE");
+                        break;
+                    }
                 }
                 Err(_) => {
                     println!("Err");
+                    break;
                 }
             }
         }
